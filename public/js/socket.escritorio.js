de4d4d6982983d01a4c17166043ca1a30eb10253
Eliana Arenas <elianaarenasm@gmail.com>
2019-10-31 16:04:57 -0500

    Primer commit -Produccion

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,30 @@
+var socket = io();
+
+var serchParams = new URLSearchParams(window.location.search);
+
+if(!serchParams.has('escritorio')){
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+    
+}
+
+var label = $('small');
+var escritorio = serchParams.get('escritorio');
+
+console.log(escritorio);
+
+$('h1').text('Escritorio'+escritorio);
+
+
+$('button').on('click',function(){
+
+    socket.emit('atenderTicket',{escritorio:escritorio},function(resp){
+      
+        if(resp === 'No hay tickets'){
+            label.text(resp);
+            alert(resp);
+            return;
+        }
+        label.text('Ticket '+ resp.numero);
+    });
+});
---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,27 @@
+//Comando para establecer la conexion
+
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+socket.on('connect',function(){
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect',function(){
+    console.log('Desconectado del servidor');
+});
+
+//on 'estadoActual'
+socket.on('estadoActual',function(res){
+
+    console.log(res);
+    label.text(res.actual);
+});
+
+$('button').on('click', function(){
+    
+    socket.emit('siguienteTicket',null,function(siguienteTicket){
+
+        label.text(siguienteTicket);
+    });
+});
